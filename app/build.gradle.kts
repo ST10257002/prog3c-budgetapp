9328abb65357e164bbe2bdd93d347b8b76110188
ST10257002 <131689755+ST10257002@users.noreply.github.com>
2025-05-22 21:36:56 +0200

    fix(build): add correct version information after build regression

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "vc.prog3c.poe"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "post-2025w21a"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

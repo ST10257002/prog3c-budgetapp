0dc0336ae328fd91202b140589bf66c7f74b2bbb
musa <ST10293362@vcconnect.edu.za>
2025-05-27 13:37:01 +0200

    big UI update

--- app/src/main/java/vc/prog3c/poe/core/utils/TestData.kt
+++ app/src/main/java/vc/prog3c/poe/core/utils/TestData.kt
@@ -1,7 +1,6 @@
 package vc.prog3c.poe.utils
 
 import android.util.Log
-import vc.prog3c.poe.data.models.CardDetails
 import vc.prog3c.poe.data.models.User
 import vc.prog3c.poe.data.services.FirestoreService
 
@@ -12,15 +11,9 @@ object TestData {
             id = "testUser1234",
             email = "testdata@gmail.com",
             name = "Test Dev User",
-            address = "123 Testing Lane",
-            phoneNumber = "082 111 5555",
+
             profilePictureUrl = "",
-            cardDetails = CardDetails(
-                cardNumber = "4111 1111 1111 1111",
-                cardType = "Visa",
-                cvc = "123",
-                expiryDate = "12/28"
-            )
+
         )
 
         // Write test
